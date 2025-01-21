4917e75f1fd60324eb6ea69b59a7a16d3a2d4851
tayadj <tayadj@users.noreply.github.com>
2025-01-21 16:34:08 +0300

    Added tests

--- project/data/extract.py
+++ project/data/extract.py
@@ -8,15 +8,15 @@ keywords = {
     ],
     'cpp': [
         'alignas', 'alignof', 'and', 'and_eq', 'asm', 'atomic_cancel', 'atomic_commit', 'atomic_noexcept', 'auto', 
-        'bitand', 'bitor', 'bool', 'break', 'case', 'catch', 'char', 'char8_t', 'char16_t', 'char32_t', 'class', 
-        'compl', 'concept', 'const', 'consteval', 'constexpr', 'constinit', 'const_cast', 'continue', 'co_await', 
+        'bitand', 'bitor', 'bool', 'break', 'case', 'catch', 'char', 'char8_t', 'char16_t', 'char32_t', 'cin', 'class', 
+        'compl', 'concept', 'const', 'consteval', 'constexpr', 'constinit', 'const_cast', 'continue', 'cout', 'co_await', 
         'co_return', 'co_yield', 'decltype', 'default', 'delete', 'do', 'double', 'dynamic_cast', 'else', 'enum', 
-        'explicit', 'export', 'extern', 'false', 'float', 'for', 'friend', 'goto', 'if', 'inline', 'int', 'long', 
+        'explicit', 'export', 'extern', 'false', 'float', 'for', 'friend', 'goto', '#include', 'if', 'inline', 'int', 'long', 
         'mutable', 'namespace', 'new', 'noexcept', 'not', 'not_eq', 'nullptr', 'operator', 'or', 'or_eq', 'private', 
         'protected', 'public', 'reflexpr', 'register', 'reinterpret_cast', 'requires', 'return', 'short', 'signed', 
         'sizeof', 'static', 'static_assert', 'static_cast', 'struct', 'switch', 'synchronized', 'template', 'this', 
         'thread_local', 'throw', 'true', 'try', 'typedef', 'typeid', 'typename', 'union', 'unsigned', 'using', 
-        'virtual', 'void', 'volatile', 'wchar_t', 'while', 'xor', 'xor_eq', 'cout', 'cin'
+        'virtual', 'void', 'volatile', 'wchar_t', 'while', 'xor', 'xor_eq'
     ],
     'java': [
         'abstract', 'assert', 'boolean', 'break', 'byte', 'case', 'catch', 'char', 'class', 'const', 'continue', 
